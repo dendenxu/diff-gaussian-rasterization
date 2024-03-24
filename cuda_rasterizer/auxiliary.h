da92611d680c5add64bdea4af6873c95e15617ec
xuzhen <zhenx@zju.edu.cn>
2024-03-24 16:23:02 +0800

    update aux computation

--- cuda_rasterizer/auxiliary.h
+++ cuda_rasterizer/auxiliary.h
@@ -149,7 +149,7 @@ __forceinline__ __device__ bool in_frustum(int idx,
 	const float* viewmatrix,
 	const float* projmatrix,
 	bool prefiltered,
-	float3& p_view,
+	float3& p_view, // reference
 	const float padding = 0.01f, // padding in ndc space // TODO: add api for changing this
 	const float xy_padding = 0.5f // padding in ndc space // TODO: add api for changing this
 	)
@@ -161,7 +161,7 @@ __forceinline__ __device__ bool in_frustum(int idx,
 	float4 p_hom = transformPoint4x4(p_orig, projmatrix);
 	float p_w = 1.0f / (p_hom.w + 0.0000001f);
 	float3 p_proj = { p_hom.x * p_w, p_hom.y * p_w, p_hom.z * p_w };
-	// p_view = transformPoint4x3(p_orig, viewmatrix); // write this outside
+	p_view = transformPoint4x3(p_orig, viewmatrix); // write this outside
 
 	return (p_proj.z > -1 - padding) && (p_proj.z < 1 + padding) && (p_proj.x > -1 - xy_padding) && (p_proj.x < 1. + xy_padding) && (p_proj.y > -1 - xy_padding) && (p_proj.y < 1. + xy_padding);
 }
